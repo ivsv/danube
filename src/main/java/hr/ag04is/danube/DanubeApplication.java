1c63480ff96f496007c27376fa9a293c2fb5d2ed
ivsv <38960994+ivsv@users.noreply.github.com>
2018-05-04 23:22:07 +0200

    first commit
    first commit

---
+++ src/main/java/hr/ag04is/danube/DanubeApplication.java
@@ -0,0 +1,12 @@
+package hr.ag04is.danube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanubeApplication.class, args);
+	}
+}
